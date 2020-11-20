69542282b6ea1b68e8db3c8c9ff5e19d90a12edf
Mario Sarcher <msarcher@mirantis.com>
2020-11-20 11:21:45 +0100

    Migrate get-org function to use mysql database as storage backend

--- lc-get-orgs/handler.go
+++ lc-get-orgs/handler.go
@@ -31,7 +31,7 @@ import (
 	"net/http"
 	"os"
 
-	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
 
 	"github.com/openfaas/openfaas-cloud/sdk"
@@ -44,18 +44,17 @@ var cors string
 // the function will panic if it cannot establish a link and the
 // container will restart / go into a crash/back-off loop
 func init() {
-	password, _ := sdk.ReadSecret("postgres-password")
-	user, _ := sdk.ReadSecret("postgres-username")
-	host, _ := sdk.ReadSecret("postgres-host")
+	password, _ := sdk.ReadSecret("mysql-password")
+	user, _ := sdk.ReadSecret("mysql-username")
+	host, _ := sdk.ReadSecret("mysql-host")
 
-	dbName := os.Getenv("postgres_db")
-	port := os.Getenv("postgres_port")
-	sslmode := os.Getenv("postgres_sslmode")
+	dbName := os.Getenv("mysql_db")
+	port := os.Getenv("mysql_port")
 
-	connStr := "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + dbName + "?sslmode=" + sslmode
+	connStr := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName
 
 	var err error
-	db, err = sql.Open("postgres", connStr)
+	db, err = sql.Open("mysql", connStr)
 
 	if err != nil {
 		panic(err.Error())
